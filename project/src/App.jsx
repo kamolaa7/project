751b180ed534a9c6a2630c925cc39e86c39e257f
amin ahmed <48972428+aminahmed65@users.noreply.github.com>
2025-07-22 17:42:38 +0000

    installed tailwind, vite, react

---
+++ project/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="border-2 border-green-500 p-5 m-4">
+        <h1 className="font-mono text-red-500 text-3xl underline">Amin</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
